7a349759d17d4dd05d6b2e46260e8ceb9309259b
Mathis Miloch <mathis.miloch@etu.univ-poitiers.fr>
2023-10-04 17:09:29 +0200

    Liste de personnage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
+
 import logo from './logo.svg';
 import './App.css';
-
-import React from "react";
-
-function Title({ color = 'red', children, hidden = false, ...props }) {
-  if (hidden) {
-      return null;
-  }
-
-  return (<h1 style={{color: color}} {...props}>{children}</h1>);
-}
+import ListeCharacters from './component/liste';
+import Title from './component/titre';
+import NumberCharacters from './component/compteur';
 
 function App() {
 return (
-  <>
-    {/* 
-        La propriété id et data-demo sont passées au composant Title grâce au spread operator.
-        La propriété color est définie dans le composant Title
-     */}
-    <Title> Hello world </Title>
-
-  </>
+ <>
+ {/* 
+ La propriété id et data-demo sont passées au composant Title grâce au spread operator.
+ La propriété color est définie dans le composant Title
+ */}
+ <Title color="black" id="my-id" data-demo="demo">Liste de personnage</Title>
+ <ListeCharacters></ListeCharacters>
+<NumberCharacters/>
+</>
 );
 }
 
